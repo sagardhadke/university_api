6239acd7299e4ea6623ce891eb26e3c3a96c4585
Sagar Dhadke <peaceunique2003@gmail.com>
2024-09-27 18:46:31 +0530

    Added MVC and other Screens

---
+++ lib/view/universityApi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyUniversityApi extends StatefulWidget {
+  const MyUniversityApi({super.key});
+
+  @override
+  State<MyUniversityApi> createState() => _MyUniversityApiState();
+}
+
+class _MyUniversityApiState extends State<MyUniversityApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
